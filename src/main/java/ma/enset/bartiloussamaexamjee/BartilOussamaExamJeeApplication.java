d9b57abb76510dd6d66814743fcc2297469938e8
oussamabartil <oussamabartil.04@gmail.com>
2025-05-19 08:02:47 +0000

    first commit

---
+++ src/main/java/ma/enset/bartiloussamaexamjee/BartilOussamaExamJeeApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.bartiloussamaexamjee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BartilOussamaExamJeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BartilOussamaExamJeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/bartiloussamaexamjee/BartilOussamaExamJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.bartiloussamaexamjee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BartilOussamaExamJeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
